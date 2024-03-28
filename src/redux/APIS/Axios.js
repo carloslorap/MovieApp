7492594648f24560a1f135cbd9ba501545e2cafc
carloslorap <74977007@certus.edu.pe>
2024-03-28 12:56:16 -0500

    ultimo

--- src/redux/APIS/Axios.js
+++ src/redux/APIS/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios =axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://movieserver-km41.onrender.com/api"
 })
 
 export default Axios
